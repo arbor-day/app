c302a98153e5436261c992f9b47a7437570630f2
joeyklee <jsphknglee@gmail.com>
2019-12-05 13:03:15 -0500

    adds geo to mapView component

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,17 +1,67 @@
 <template>
-  <div id="map-view"></div>
+  <div id="map-view">
+    <p style="display:none">{{this.todos}}</p>
+  </div>
 </template>
 
 <script>
 
-// import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 // import * as turf from '@turf/turf';
 
 export default {
   name: 'map-view',
-  methods:{},
+  data(){
+    return{
+      geo: null,
+    }
+  },
+  computed:{
+    todos(){
+      const markers = this.$store.getters.todoMarkers
+      if (this.geo !== null && this.geo.loaded()) {
+        this.geo.getSource("todo-locations").setData(markers);
+      }
+      return markers;
+    }
+  },
+  methods:{
+    initMap(mapId) {
+      mapboxgl.accessToken =
+        "pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw";
+
+      this.geo = new mapboxgl.Map({
+        container: mapId,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-73.975866, 40.676966], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+      });
+
+      this.geo.on("load", () => {
+        this.geo.addSource("todo-locations", {
+          type: "geojson",
+          data: this.$store.getters.todoMarkers
+        });
+
+        this.geo.addLayer({
+          id: "todo-locations",
+          type: "circle",
+          source: "todo-locations",
+          paint: {
+            "circle-radius": 10,
+            "circle-color": "#ff0000"
+          }
+        });
+      });
+    },
+    // updatePoints() {
+    //   if (this.geo !== null && this.geo.loaded()) {
+    //     this.geo.getSource("todo-locations").setData(this.todos);
+    //   }
+    // }
+  },
   mounted() {
-    this.$store.commit('initMap', 'map-view');
+    this.initMap('map-view');
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
+// import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 import * as turf from "@turf/turf";
 
 Vue.use(Vuex);
@@ -18,7 +18,7 @@ export default new Vuex.Store({
         description: "an empty spot on the sidewalk"
       }
     ],
-    geo: null
+    // geo: null
   },
   getters: {
     todos: state => {
@@ -40,51 +40,18 @@ export default new Vuex.Store({
     },
     removeTodo(state, id) {
       state.todos = state.todos.filter(item => item.id !== id);
-    },
-    initMap(state, mapId) {
-      mapboxgl.accessToken =
-        "pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw";
-
-      state.geo = new mapboxgl.Map({
-        container: mapId,
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: [-73.975866, 40.676966], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-      });
-
-      state.geo.on("load", () => {
-        state.geo.addSource("todo-locations", {
-          type: "geojson",
-          data: this.getters.todoMarkers
-        });
-
-        state.geo.addLayer({
-          id: "todo-locations",
-          type: "circle",
-          source: "todo-locations",
-          paint: {
-            "circle-radius": 10,
-            "circle-color": "#ff0000"
-          }
-        });
-      });
-    },
-    updatePoints(state) {
-      if (state.geo !== null && state.geo.loaded()) {
-        state.geo.getSource("todo-locations").setData(this.getters.todoMarkers);
-      }
     }
   },
   actions: {
     async addTodo(context, todo) {
       // console.log("add todo");
       context.commit("addTodo", todo);
-      context.commit("updatePoints");
+      // context.commit("updatePoints");
     },
     async removeTodo(context, id) {
       // console.log("remove todo");
       context.commit("removeTodo", id);
-      context.commit("updatePoints");
+      // context.commit("updatePoints");
     }
   },
   modules: {}
