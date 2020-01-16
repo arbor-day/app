4befca7510104f6b899b578379c36973078a23b1
joeyklee <jsphknglee@gmail.com>
2020-01-16 00:40:22 -0500

    updates homepage / rm how section temporarily

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,7 @@
       </div>
     </header>
     <main class="main">
+      <!-- section project -->
       <section class="section__project">
         <h2 class="section__title">The Project</h2>
         <div class="section__body">
@@ -23,16 +24,16 @@
           </div>
         </div>
       </section>
-
-      <section class="section__how">
-        <h2 class="section__title">How</h2>
+      <!-- section how -->
+      <!-- <section class="section__how">
+        <h2 class="section__title">Making Connections</h2>
         <div class="section__articles">
           <article class="section__article">
             <header class="section__article__header section__article__header--right">
               <div class="section__article__header__container">
                 <h2 class="section__article__header__title">Connecting with the City</h2>
                 <p class="section__article__header__text">
-                  Whether you're new to the city or native New Yorker, we all have one thing in common: we spend a lot of time walking around the city.
+                  Whether you're new to the city or a native New Yorker, we all have one thing in common: we spend a lot of time walking around.
                   You can help the MoreTrees project by keeping your eyes 👀 out for empty tree beds and submitting them to our database.
                   You can learn more about ways of <a href="#"> engaging with the city here</a>.
                 </p>
@@ -40,7 +41,6 @@
             </header>
             <div class="section__article__image section__article__image--city"></div>
           </article>
-
           <article class="section__article">
             <header class="section__article__header section__article__header--left">
               <div class="section__article__header__container">
@@ -54,7 +54,6 @@
             </header>
             <div class="section__article__image section__article__image--you"></div>
           </article>
-
           <article class="section__article">
             <header class="section__article__header section__article__header--right">
               <div class="section__article__header__container">
@@ -68,8 +67,75 @@
             <div class="section__article__image section__article__image--environment"></div>
           </article>
         </div>
+      </section> -->
+
+      <!-- section operations -->
+      <!-- <section class="section__operations">
+        <h2 class="section__title">How</h2>
+        <div class="section__text">
+          &nbsp;
+        </div>
+      </section> -->
+      <!--  -->
+      <section class="section">
+        <h2 class="section__title">FAQ</h2>
+        <div class="faq">
+          <!--  -->
+          <article class="faq__container">
+           <p class="faq__question"><span class="faq__symbol">+</span> Who funds MoreTrees?</p>
+           <p class="faq__answer"><span class="faq__symbol">↳</span> MoreTrees is a side project by Andy and Joey. The operating costs of the web server, the organization time, etc are all done without compensation. If you'd like to support the project, feel free to holler!</p> 
+          </article>
+          <!--  -->
+          <article class="faq__container">
+           <p class="faq__question"><span class="faq__symbol">+</span> Is MoreTrees part of the city government?</p>
+           <p class="faq__answer"><span class="faq__symbol">↳</span> MoreTrees is NOT part of the city government. It might broadly be categorized as a civic engagement from concerned/curious citizens. In other words, we're just two people (and friends) who want to make it easier for the city to plant trees.</p> 
+          </article>
+          <!--  -->
+          <article class="faq__container">
+           <p class="faq__question"><span class="faq__symbol">+</span> Are there opportunities to contribute?</p>
+           <p class="faq__answer"><span class="faq__symbol">↳</span> There sure are! There are many ways to get involved. You can be a mapper, organize a planting, become a steward, or just spread the word. Any and every contribution is meaningful.</p> 
+          </article>
+          <!--  -->
+          <article class="faq__container">
+           <p class="faq__question"><span class="faq__symbol">+</span> Can I make a donation?</p>
+           <p class="faq__answer"><span class="faq__symbol">↳</span> You can donate to support our server costs or donate money to any of these fine organizations...</p> 
+          </article>
+          <!--  -->
+          <article class="faq__container">
+           <p class="faq__question"><span class="faq__symbol">+</span> Does the city really plant trees for free?</p>
+           <p class="faq__answer"><span class="faq__symbol">↳</span> Yes! This is why this project started. We wanted a way to continually work towards identifying empty tree beds where the city could plant trees throughout New York.</p> 
+          </article>
+        </div>
       </section>
     </main>
+    <footer class="footer">
+      <div class="footer__container">
+        <nav class="footer__nav">
+          <section class="footer__section">
+            <ul class="footer__nav-list">
+              <li class="footer__nav-list__item">about</li>
+              <li class="footer__nav-list__item">contact</li>
+              <li class="footer__nav-list__item">submit</li>
+            </ul>
+          </section>
+          <section class="footer__section">
+            <ul class="footer__nav-list">
+              <li class="footer__nav-list__item">privacy</li>
+              <li class="footer__nav-list__item">code of conduct</li>
+              <li class="footer__nav-list__item">terms & conditions</li>
+            </ul>
+          </section>
+          <section class="footer__section">
+            <ul class="footer__nav-list">
+              <li class="footer__nav-list__item">contribute</li>
+              <li class="footer__nav-list__item">donate</li>
+              <li class="footer__nav-list__item">events</li>
+            </ul>
+          </section>
+        </nav>
+        <p class="footer__copy">© 2019 MoreTrees.NYC, Brooklyn, NY</p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -86,8 +152,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.footer{
+  display: flex;
+  flex-direction:column;
+  padding:0.5rem 0rem;
+  height: 16rem;
+  width:100%;
+
+  &__container{
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction: column;
+    background-color: black;
+    padding-bottom:0.5rem;
+  }
+
+  &__copy{
+    font-size:0.9rem;
+    color:white;
+  }
+
+  &__nav{
+    
+    width:100%;
+    height:100%;
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 0.5rem;
+    align-items: center;
+  }
+
+  &__section{
+    text-align:center;
+  }
+
+  &__nav-list{
+    list-style: none;
+
+    &__item{
+      font-size:1.2rem;
+      color:white;
+    }
+  }
+}
+
 .header {
-  background-color: aliceblue;
+  // background-color: aliceblue;
+  border:2px solid black;
   background-size: cover;
   height: 90vh;
   display: flex;
@@ -114,7 +227,7 @@ export default {
     display: flex;
 
     &__button {
-      border: 4px white;
+      border: 2px solid black;
       padding: 1rem 1.2rem;
       text-transform: uppercase;
       letter-spacing: 0.2ch;
@@ -137,12 +250,46 @@ export default {
 .main {
   margin: 2rem 0;
 
+  .faq {
+    max-width: 600px;
+    margin:0 auto;
+
+    &__container{
+      display:flex;
+      flex-direction:column;
+      text-align:left;
+      margin-bottom:1rem;
+      border:2px solid black;
+    }
+
+    &__question{
+      font-size:1.6rem;
+      font-weight: bold;
+      padding: 0.5rem 0.25rem;
+      background-color:black;
+      color:white;
+    }
+    &__answer{
+      font-size:1.4rem;
+      padding: 0.5rem 0.25rem;
+      margin-left:0.25rem;
+      margin-top:0.25rem;
+      background-color:white;
+      color:black;
+    }
+
+    &__symbol{
+      font-weight: bold;
+    }
+  }
+
   .section {
     &__title {
       font-size: 2.6rem;
       width: 100%;
       position: relative;
       color: white;
+      margin-bottom:2rem;
     }
 
     &__title::before {
@@ -151,7 +298,7 @@ export default {
       top: 0;
       left: 50%;
       transform: translate(-50%, 0%);
-      width: 300px;
+      width: 600px;
       height: 100%;
       border: 2px solid black;
       background-color: black;
