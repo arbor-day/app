f65bba5bf17a0d966f20d2c98b2f41765bb0d7c3
joeyklee <jsphknglee@gmail.com>
2020-01-16 00:48:45 -0500

    code cleanup

---
+++ src/components/Footer.vue
@@ -0,0 +1,84 @@
+<template>
+  <footer class="footer">
+      <div class="footer__container">
+        <nav class="footer__nav">
+          <section class="footer__section">
+            <ul class="footer__nav-list">
+              <li class="footer__nav-list__item">about</li>
+              <li class="footer__nav-list__item">contact</li>
+              <li class="footer__nav-list__item">submit</li>
+            </ul>
+          </section>
+          <section class="footer__section">
+            <ul class="footer__nav-list">
+              <li class="footer__nav-list__item">privacy</li>
+              <li class="footer__nav-list__item">code of conduct</li>
+              <li class="footer__nav-list__item">terms & conditions</li>
+            </ul>
+          </section>
+          <section class="footer__section">
+            <ul class="footer__nav-list">
+              <li class="footer__nav-list__item">contribute</li>
+              <li class="footer__nav-list__item">donate</li>
+              <li class="footer__nav-list__item">events</li>
+            </ul>
+          </section>
+        </nav>
+        <p class="footer__copy">© 2019 MoreTrees.NYC, Brooklyn, NY</p>
+      </div>
+    </footer>
+</template>
+<script>
+export default {
+  name: "footer"
+};
+</script>
+
+<style scoped lang="scss">
+
+.footer{
+  display: flex;
+  flex-direction:column;
+  padding:0.5rem 0rem;
+  height: 16rem;
+  width:100%;
+
+  &__container{
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction: column;
+    background-color: black;
+    padding-bottom:0.5rem;
+  }
+
+  &__copy{
+    font-size:0.9rem;
+    color:white;
+  }
+
+  &__nav{
+    
+    width:100%;
+    height:100%;
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 0.5rem;
+    align-items: center;
+  }
+
+  &__section{
+    text-align:center;
+  }
+
+  &__nav-list{
+    list-style: none;
+
+    &__item{
+      font-size:1.2rem;
+      color:white;
+    }
+  }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -79,124 +79,29 @@
       <!--  -->
       <section class="section">
         <h2 class="section__title">FAQ</h2>
-        <div class="faq">
-          <!--  -->
-          <article class="faq__container">
-           <p class="faq__question"><span class="faq__symbol">+</span> Who funds MoreTrees?</p>
-           <p class="faq__answer"><span class="faq__symbol">↳</span> MoreTrees is a side project by Andy and Joey. The operating costs of the web server, the organization time, etc are all done without compensation. If you'd like to support the project, feel free to holler!</p> 
-          </article>
-          <!--  -->
-          <article class="faq__container">
-           <p class="faq__question"><span class="faq__symbol">+</span> Is MoreTrees part of the city government?</p>
-           <p class="faq__answer"><span class="faq__symbol">↳</span> MoreTrees is NOT part of the city government. It might broadly be categorized as a civic engagement from concerned/curious citizens. In other words, we're just two people (and friends) who want to make it easier for the city to plant trees.</p> 
-          </article>
-          <!--  -->
-          <article class="faq__container">
-           <p class="faq__question"><span class="faq__symbol">+</span> Are there opportunities to contribute?</p>
-           <p class="faq__answer"><span class="faq__symbol">↳</span> There sure are! There are many ways to get involved. You can be a mapper, organize a planting, become a steward, or just spread the word. Any and every contribution is meaningful.</p> 
-          </article>
-          <!--  -->
-          <article class="faq__container">
-           <p class="faq__question"><span class="faq__symbol">+</span> Can I make a donation?</p>
-           <p class="faq__answer"><span class="faq__symbol">↳</span> You can donate to support our server costs or donate money to any of these fine organizations...</p> 
-          </article>
-          <!--  -->
-          <article class="faq__container">
-           <p class="faq__question"><span class="faq__symbol">+</span> Does the city really plant trees for free?</p>
-           <p class="faq__answer"><span class="faq__symbol">↳</span> Yes! This is why this project started. We wanted a way to continually work towards identifying empty tree beds where the city could plant trees throughout New York.</p> 
-          </article>
-        </div>
+        <FAQ/>
       </section>
     </main>
-    <footer class="footer">
-      <div class="footer__container">
-        <nav class="footer__nav">
-          <section class="footer__section">
-            <ul class="footer__nav-list">
-              <li class="footer__nav-list__item">about</li>
-              <li class="footer__nav-list__item">contact</li>
-              <li class="footer__nav-list__item">submit</li>
-            </ul>
-          </section>
-          <section class="footer__section">
-            <ul class="footer__nav-list">
-              <li class="footer__nav-list__item">privacy</li>
-              <li class="footer__nav-list__item">code of conduct</li>
-              <li class="footer__nav-list__item">terms & conditions</li>
-            </ul>
-          </section>
-          <section class="footer__section">
-            <ul class="footer__nav-list">
-              <li class="footer__nav-list__item">contribute</li>
-              <li class="footer__nav-list__item">donate</li>
-              <li class="footer__nav-list__item">events</li>
-            </ul>
-          </section>
-        </nav>
-        <p class="footer__copy">© 2019 MoreTrees.NYC, Brooklyn, NY</p>
-      </div>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
+import FAQ from "@/components/FAQ.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "home",
   components: {
-    // HelloWorld
+    FAQ,
+    Footer
   }
 };
 </script>
 
 <style lang="scss" scoped>
 
-.footer{
-  display: flex;
-  flex-direction:column;
-  padding:0.5rem 0rem;
-  height: 16rem;
-  width:100%;
-
-  &__container{
-    width:100%;
-    height:100%;
-    display:flex;
-    flex-direction: column;
-    background-color: black;
-    padding-bottom:0.5rem;
-  }
-
-  &__copy{
-    font-size:0.9rem;
-    color:white;
-  }
-
-  &__nav{
-    
-    width:100%;
-    height:100%;
-    display:grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-column-gap: 0.5rem;
-    align-items: center;
-  }
-
-  &__section{
-    text-align:center;
-  }
-
-  &__nav-list{
-    list-style: none;
-
-    &__item{
-      font-size:1.2rem;
-      color:white;
-    }
-  }
-}
 
 .header {
   // background-color: aliceblue;
@@ -250,39 +155,7 @@ export default {
 .main {
   margin: 2rem 0;
 
-  .faq {
-    max-width: 600px;
-    margin:0 auto;
-
-    &__container{
-      display:flex;
-      flex-direction:column;
-      text-align:left;
-      margin-bottom:1rem;
-      border:2px solid black;
-    }
-
-    &__question{
-      font-size:1.6rem;
-      font-weight: bold;
-      padding: 0.5rem 0.25rem;
-      background-color:black;
-      color:white;
-    }
-    &__answer{
-      font-size:1.4rem;
-      padding: 0.5rem 0.25rem;
-      margin-left:0.25rem;
-      margin-top:0.25rem;
-      background-color:white;
-      color:black;
-    }
-
-    &__symbol{
-      font-weight: bold;
-    }
-  }
-
+  
   .section {
     &__title {
       font-size: 2.6rem;
