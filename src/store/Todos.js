917cbd389e8c81b0318a40d89e20cb48204f8e6e
joeyklee <jsphknglee@gmail.com>
2020-03-02 22:07:38 -0500

    update error handling

--- src/store/Todos.js
+++ src/store/Todos.js
@@ -82,7 +82,7 @@ export default {
         context.commit("addTodo", result);
         context.commit("updatePoints");
       } catch (error) {
-        alert(`Uh-oh! Something went wrong. \n error: ${JSON.stringify}`);
+        alert(`Uh-oh! Something went wrong. \n error: ${JSON.stringify(error)}`);
         throw new Error(error);
       }
     },
